82f3abc85e3d8245dc7cdd7a8c6d2f40ba27fd3e
Pellennen <pelle_9z@gotmail.com>
2020-05-03 16:02:48 +0200

    added burgermenu on smaller devices

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
-
-
-$('.hide-selections').hide()
+  $(".hide-selections").hide();
 
   //Toggles between hidden and shown with easeing
 
-  $('.cat-burger').on('click', function(){
-    $('.hide-selections').toggle('fast')
-  })
+  $(".cat-burger").on("click", function() {
+    $(".hide-selections").toggle("fast");
+  });
 
+  $(".burger-menu").on("click", function() {
+    $(this).hide();
+    $('.sidenav').css('width', '250px');
+  });
+
+  $('.closebtn').on('click', function() {
+    $('.burger-menu').show();
+    $('.sidenav').css('width', '0');
+
+  })
 });
